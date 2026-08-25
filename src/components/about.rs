50b92cb2724495d7766f9e3b2badd139d488f6da
Sreenand P K <sreenandpk3@gmail.com>
2026-08-26 00:16:58 +0700

    content: Remove 'ME' from About heading

--- src/components/about.rs
+++ src/components/about.rs
@@ -7,7 +7,7 @@ pub fn About() -> Element {
             div { class: "container",
                 div { class: "about-split-layout",
                     div { class: "about-heading-left",
-                        h2 { class: "about-huge-title-left", "ABOUT ME" }
+                        h2 { class: "about-huge-title-left", "ABOUT" }
                     }
                     
                     div { class: "about-content-right",
